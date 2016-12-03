d1f816283e7d34ddc7f8a2462f22bc3de5e177b9
alumnoIdat <leomanarael@gmail.com>
2016-12-03 03:13:20 -0500

    a medias

--- app/src/main/java/com/example/chango/googlemapsapi/MainActivity.java
+++ app/src/main/java/com/example/chango/googlemapsapi/MainActivity.java
@@ -3,43 +3,46 @@ package com.example.chango.googlemapsapi;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.os.Bundle;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends android.support.v4.app.FragmentActivity implements OnMapReadyCallback{
 
-    private GoogleMap mMap;
+    private GoogleMap gMap;
+    private CameraUpdate mCamera;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setUpMapIfNeeded();
-    }
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
-    }
-
-    private void setUpMapIfNeeded() {
-        if (mMap == null) {
-            mMap = ((SupportMapFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.map)).getMap();
-            if (mMap != null) {
-                mMap.setMyLocationEnabled(true);
-                setUpMap();
-            }
-        }
+        mapFragment.getMapAsync(this);
     }
 
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(-12.017124,
-                -77.050744)).title("Facultad de Ciencias"));
+        gMap.addMarker(new MarkerOptions().position(new LatLng(-12.017124,
+                -77.050744)).title("Facultad de Ciencias").icon(BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_CYAN))
+                .snippet("The beast School"));
+        mCamera = CameraUpdateFactory.newLatLngZoom(new LatLng(
+                -12.017124, -77.050744), 0);
+        gMap.animateCamera(mCamera);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+        setUpMap();
+    }
 }
