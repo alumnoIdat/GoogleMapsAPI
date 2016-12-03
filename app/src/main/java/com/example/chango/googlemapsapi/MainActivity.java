8e5b442a0173d23ad17d067b90af330a3a5a60d8
alumnoIdat <leomanarael@gmail.com>
2016-12-03 01:19:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/chango/googlemapsapi/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.chango.googlemapsapi;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            mMap = ((SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map)).getMap();
+            if (mMap != null) {
+                mMap.setMyLocationEnabled(true);
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-12.017124,
+                -77.050744)).title("Facultad de Ciencias"));
+    }
+
+}
